25ff856f159802f4a11d979700dd44b5d510e77c
Maryna Ryshkevych <mary7r8@gmail.com>
2026-07-25 20:40:04 +0300

    fix for loop and set bound for MachineProducer interface

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,13 +1,9 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineProducer<T> {
-    
-    /**
-     * In your implementations - create 2-3 models, add them to the list and return
-     * @return - the list of models
-     */
+public interface MachineProducer<T extends Machine> {
 
     List<T> get();
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,7 +4,6 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-import java.util.Collections;
 import java.util.List;
 
 public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
@@ -26,7 +25,7 @@ public class MachineServiceImpl<T extends Machine> implements MachineService<T> 
     @Override
     public void fill(List<? super T> machines, T value) {
         for (int i = 0; i < machines.size(); i++) {
-            Collections.fill(machines, value);
+            machines.set(i, value);
         }
     }
 
