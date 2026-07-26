0a825cbc009ce400fea5fd32bc471850523cbeea
Maryna Ryshkevych <mary7r8@gmail.com>
2026-07-26 20:05:30 +0300

    fix some mistakes

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,10 +1,11 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
 import java.util.List;
 
-public class ExcavatorProducer implements MachineProducer<Excavator> {
-    public List<Excavator> get() {
+public class ExcavatorProducer implements MachineProducer<Machine> {
+    public List<Machine> get() {
         return List.of(new Excavator(), new Excavator());
     }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -6,24 +6,24 @@ import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.List;
 
-public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
-    public List<T> getAll(Class<? extends T> type) {
+    public List<Machine> getAll(Class<? extends Machine> type) {
         if (type == Bulldozer.class) {
-            return (List<T>) new BulldozerProducer().get();
+            return new BulldozerProducer().get();
         }
         if (type == Excavator.class) {
-            return (List<T>) new ExcavatorProducer().get();
+            return new ExcavatorProducer().get();
         }
         if (type == Truck.class) {
-            return (List<T>) new TruckProducer().get();
+            return new TruckProducer().get();
         }
         return List.of();
     }
 
     @Override
-    public void fill(List<? super T> machines, T value) {
+    public void fill(List<? super Machine> machines, Machine value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
